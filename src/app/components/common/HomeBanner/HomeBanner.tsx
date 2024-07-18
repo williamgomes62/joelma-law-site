d33aad3250f505d8238d383eb392aebba1d6cad0
William Natividade <william.natividade@petrosoftdesign.com>
2024-07-18 10:30:28 -0300

    feat(home banner): adiciona layout grid e gradiente

--- src/app/components/common/HomeBanner/HomeBanner.tsx
+++ src/app/components/common/HomeBanner/HomeBanner.tsx
@@ -5,60 +5,23 @@ export default function HomeBanner() {
   return (
     <>
       <div className="relative isolate px-6 pt-14 lg:px-8 bg-white banner">
-        {/* <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-          />
-        </div> */}
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          {/* <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-white ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{' '}
-              <a href="#" className="font-semibold text-indigo-600">
-                <span className="absolute inset-0" aria-hidden="true" />
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
-          </div> */}
-          {/* <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-              Trabalho para defender seus Direitos
-            </h1>
-            <p className="mt-6 text-lg leading-8 text-white">
-              Sou advogada com mais de 10 anos de experiência, atuante em processos judiciais e extrajudiciais.
-              Atuante em Direito da Saúde, Violência Obstétrica,Erro Médico, em favor dos consumidores/Pacientes e Benefícios Assistenciais- BPC/LOAS.
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                ENTRAR EM CONTATO
-              </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-white">
-                Learn more <span aria-hidden="true">→</span>
-              </a>
-            </div>
-          </div> */}
-          <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="text-center py-16">
+        <div className="background-gradient"></div>
+        <img
+          src='https://lh3.googleusercontent.com/proxy/iU2s7uDIh4FL-0Gc-qkHKfwqHTbAQskFW_vExOofEWtH94wR0gwn0dSlGyXCFKtHdU4T36PTuZRg06py_uNJljYVe2OgqpJc9Iv_aLsXSt7J-o_DJV-n1-cz'
+        />
+        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 content">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="text-center py-16 col-span-2">
               <div className="animate__animated animate__fadeInLeft">
-                <h1 className="text-4xl sm:text-5xl font-bold text-white mb-4">
+                <h1 className="text-left text-4xl sm:text-5xl font-bold text-white mb-4">
                   Trabalho para defender seus Direitos
                 </h1>
-                <p className="text-lg text-white mb-6">
+                <p className="text-lg text-white mb-6 text-left">
                   Sou advogada com mais de 10 anos de experiência, atuante em processos judiciais e extrajudiciais.
                   Atuante em Direito da Saúde, Violência Obstétrica,Erro Médico, em favor dos consumidores/Pacientes e Benefícios Assistenciais- BPC/LOAS.
                 </p>
 
-                <div className="cta-container rounded-3xl flex flex-col sm:flex-row justify-center items-center space-y-4 sm:space-y-0 sm:space-x-6">
+                <div className="cta-container rounded-3xl flex flex-col sm:flex-row justify-start items-center space-y-4 sm:space-y-0 sm:space-x-6">
                   <a href="#" className="cta-button inline-block px-8 py-3 bg-blue-600 text-white font-medium text-lg rounded-3xl hover:bg-blue-700 transition duration-300">
                     ENTRAR EM CONTATO
                   </a>
