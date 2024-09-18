e141c645e0a0ad493fbd8907d1dda9a8ed755318
William Natividade <william.natividade@petrosoftdesign.com>
2024-09-17 22:19:10 -0300

    fix(routes): adjuste pages directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 // import 'tailwindcss/tailwind.css';
-import BpcLoasPage from "./pages/bpc-loas";
 import Home from "./pages/Home";
 
 export default function App() {
   return (
     <>
       <Home />
-      {/* <BpcLoasPage /> */}
     </>
   );
 }
